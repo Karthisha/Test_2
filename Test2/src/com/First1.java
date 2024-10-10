aad5bacf8d463105865fb77ed1d59aefdbf84f5b
Karthisha <116002042+Karthisha@users.noreply.github.com>
2024-10-10 16:25:05 +0530

    First commit

---
+++ Test2/src/com/First1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * 
+ */
+public class First1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
